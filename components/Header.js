738607f9593ae2d3c8615839a16e84db0af0972e
Gabriel Mares <henry_gm@hotmail.com>
2021-06-04 12:27:16 -0700

    agregando fuente poppins

--- components/Header.js
+++ components/Header.js
@@ -5,10 +5,9 @@ import styles from '../styles/Home.module.css'
 const Encabezado = () => {
     const { height } = useWindow()
     const estilosEncabezado = (height * 1.2 >= window.innerHeight ) ? styles.conGradiente : styles.encabezado
-    console.log(height)
     return (
         <nav className={estilosEncabezado} >
-            <span className={styles.title}>GABRIEL MARES</span>
+            <span className={styles.title}>Gabriel Mares</span>
             <ul className={styles.enlaces}>
                 <a>About</a>
                 <a>Proyects</a>
